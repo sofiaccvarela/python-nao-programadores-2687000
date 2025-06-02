385876e1f4d3905979d707b235eb5f95a69ea4dc
sofiaccvarela <sofia-c-varela@telecom.pt>
2025-06-02 13:33:59 +0000

    resolucao exercicio 2.3

--- exercicios/2-estruturas-de-dados/2-dicionario.py
+++ exercicios/2-estruturas-de-dados/2-dicionario.py
@@ -7,12 +7,16 @@ pessoa = {'nome':'Crislaine',
           'animal_estimacao':False}
 
 # Imprima na tela o valor equivalente a chave "hobby"
-print(pessoa.hobby)
+print(pessoa['hobby'])
 
 # Imprima na tela uma lista apenas com os valores do dicionário
-
+valores = list(pessoa.values())
+print(valores)
 
 # Imprima na tela uma lista apenas com as chaves do dicionário
-
+chaves = list(pessoa.keys())
+print(chaves)
 
 # Insira um novo par chave-valor no dicionário
+pessoa['altura'] = 1.74
+print(pessoa)
