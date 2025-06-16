4b020a9de469c7949a34b20ee621dd9b9820550c
sofiaccvarela <sofia-c-varela@telecom.pt>
2025-06-16 15:48:22 +0000

    resolucao exercicio 3.4

--- exercicios/3-estruturas-de-controle/projeto-fizz-buzz.py
+++ exercicios/3-estruturas-de-controle/projeto-fizz-buzz.py
@@ -6,3 +6,24 @@
 # 3.2 Caso o número seja divisível por 5, substitua-o por "Buzz"
 # 3.3 Caso o número seja divisível por 3 e 5, substitua-o por "FizzBuzz"
 
+#criação da lista
+lista = list(range(15,31))
+# variável auxiliar para indice da lista
+item=0
+#imprimimos a lista completa
+print(lista)
+
+for numero in lista:
+  if numero%3==0 and numero%5==0: 
+    lista[item]="FizzBuzz"
+  elif numero%3==0:
+    lista[item]=="Fizz"
+  elif numero%5==0:
+    lista[item]=="Buzz"
+  else: 
+   lista[item]=numero
+
+  item +=1
+
+print(lista)
+  
